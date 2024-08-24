b592319744cd94403b525d8ea1e27736ede66146
Иван <kandolov.i@gmail.com>
2024-08-24 11:58:01 +0400

    T-1 init sum method

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     Main main = new Main();
 
+    @Test
     void myFirstTest() {
         int result = main.sum(1,2);
         assertEquals(3, result);
